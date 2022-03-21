505ea48dd8b9968378567a6fc9421d7a83397395
Lucas Ricarte <lucasrrt@gmail.com>
2022-03-21 18:48:40 -0300

    Add instructions and automatize commands for nvim setup

---
+++ nvim/setup.sh
@@ -0,0 +1,7 @@
+mkdir -p ~/.config/nvim
+ln -f ./init.vim ~/.config/nvim/init.vim
+
+mkdir -p ~/.config/nvim/configs/
+ln -f ./configs/bufsel.vim ~/.config/nvim/configs/bufsel.vim
+ln -f ./configs/learn-vimscript-the-hard-way.vim ~/.config/nvim/configs/learn-vimscript-the-hard-way.vim
+ln -f ./configs/smartreplace.vim ~/.config/nvim/configs/smartreplace.vim
